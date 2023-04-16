b407b570a14953a1d83a8f9fad3d721490bfab92
unknown <thomasjjohnson9311@gmail.com>
2023-04-16 10:07:33 +0530

    final product?

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let currentDate = document.querySelector("#currentDay");
 let px5Container = document.querySelector(".px-5");
 let buttonArray = [];
 let newText = document.querySelectorAll(".description");
-let divNum = document.querySelectorAll("#py-3");
+// let divNum = document.querySelectorAll("#py-3");
 
 currentDate.textContent = dayjs().format("MMM DD, YYYY");
 
+// checks for click on the button
+// loops through class .description
+// finds text inside of element & stores that value in local storage
 px5Container.addEventListener("click", function (event) {
   if (event.target.matches("i")) {
     for (let i = 0; i < newText.length; i++) {
-      console.log(newText[i].parentNode.textContent);
+      // console.log(newText[i].parentNode.textContent);
       localStorage.setItem(newText[i].parentNode.textContent, newText[i].value);
       // resets the input area after saving
       newText[i].value = "";
@@ -17,6 +20,7 @@ px5Container.addEventListener("click", function (event) {
   }
 });
 
+//
 // changes color based on past(grey)/present(red)/future(green)
 for (let i = 0; i < 9; i++) {
   let timeEl = document.querySelector("#hour-" + (i + 9));
