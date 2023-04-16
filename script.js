98d5ac4a029f881df83658b775071be065cb4090
unknown <thomasjjohnson9311@gmail.com>
2023-04-16 00:29:34 -0500

    added condition to save on the button and i element

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ let currentDate = document.querySelector("#currentDay");
 let px5Container = document.querySelector(".px-5");
 let buttonArray = [];
 let newText = document.querySelectorAll(".description");
-// let divNum = document.querySelectorAll("#py-3");
 
-currentDate.textContent = dayjs().format("dddd, " + "MMM DD, YYYY");
+currentDate.textContent = dayjs().format("dddd, " + "MMM DD, YYYY" + " h:mm A");
 
-// checks for click on the button
+// checks for click on the i and button elements
 // loops through class .description
 // finds text inside of element & stores that value in local storage
 px5Container.addEventListener("click", function (event) {
-  if (event.target.matches("i")) {
+  if (event.target.matches("i") || event.target.matches("button")) {
     for (let i = 0; i < newText.length; i++) {
       // console.log(newText[i].parentNode.textContent);
       localStorage.setItem(newText[i].parentNode.textContent, newText[i].value);
